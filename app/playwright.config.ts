0b2a0ca947af2629b71f408f3958496dbacf7825
John Dennehy <johndennehy101@gmail.com>
2026-03-18 21:36:26 +0000

    Script and results of running model outputs against target changes (to evaluate if generated tests pass / fail)

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -2,20 +2,20 @@ import { defineConfig, devices } from "@playwright/test"
 
 export default defineConfig({
   testDir: "./e2e",
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
     baseURL: "http://localhost:5175",
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: { ...devices["Desktop Chrome"], screenshot: "on", video: "on" },
     },
   ],
 
